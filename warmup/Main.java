fbb11ade51ce07ffb2b1146357ba2210ecb309e8
Himank890 <hs8358@pleasantonusd.net>
2026-01-08 23:01:41 +0000

    Problem 1 completed

--- warmup/Main.java
+++ warmup/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         // Your code here.
-
+        int temp = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                temp += i;
+            }
+        }
+        System.out.println(temp);
     }
 }
