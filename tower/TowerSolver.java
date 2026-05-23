19d87569393361901036caed6ba3dd8f0b4c54fc
Himank890 <hs8358@pleasantonusd.net>
2026-05-23 04:24:24 +0000

    Worked on a towerSolverr code

--- tower/TowerSolver.java
+++ tower/TowerSolver.java
@@ -19,5 +19,13 @@ public class TowerSolver {
     //
     // [ solve method here]
     //
-
+    private void solveRecursive(int n, int source, int destination, int auxiliary) {
+        if (n == 1) {
+            model.move(source, destination);
+            return;
+        }
+        solveRecursive(n-1, source, auxiliary, destination);
+        model.move(source, destination);
+        solveRecursive(n-1, auxiliary, destination, source);
+    }
 }
