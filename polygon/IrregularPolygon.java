e5bed90b1ca8e423195f45cb16de6cb1e3c301d9
Himank890 <hs8358@pleasantonusd.net>
2026-05-29 06:20:21 +0000

    Fixed compiler issues

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -20,28 +20,29 @@ public class IrregularPolygon {
     public double perimeter() {
         // TODO: Calculate the perimeter.
         double totalPerimeter = 0.0;
-        int n = vertices.size();
+        int n = myPolygon.size();
 
         if (n < 3) return 0.0;
 
         for (int i = 0; i < n; i++) {
-            Point2D.Double current = vertices.get(i);
-            Point2D.Double next = vertices.get((i+1) % n);
+            Point2D.Double current = myPolygon.get(i);
+            Point2D.Double next = myPolygon.get((i+1) % n);
 
-            totalPerimeter += current.distance(next)
+            totalPerimeter += current.distance(next);
         }
+        return totalPerimeter;
     }   
 
     public double area() {
         // TODO: Calculate the area.
         Double sum = 0.0;
-        int n = vertices.size();
+        int n = myPolygon.size();
 
         if (n < 3) return 0.0;
 
         for (int i = 0; i < n; i++) {
-            Point2D.Double current = vertices.get(i);
-            Point2D.Double next = vertices.get((i+1) % n);
+            Point2D.Double current = myPolygon.get(i);
+            Point2D.Double next = myPolygon.get((i+1) % n);
 
             sum += (current.getX() * next.getY()) - (current.getY() * next.getX());
         }
@@ -56,17 +57,17 @@ public class IrregularPolygon {
             // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
             //DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
             //myDrawingTool.move(50, 50);
-            if (vertices.size() > 0) {
+            if (myPolygon.size() > 0) {
                 DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
 
-                Point2D.Double first = vertices.get(0);
+                Point2D.Double first = myPolygon.get(0);
                 myDrawingTool.up();
                 myDrawingTool.move(first.getX(), first.getY());
 
                 myDrawingTool.down();
 
-                for (int i = 1; i < vertices.size(); i++ {
-                    Point2D.Double current = vertices.get(i);
+                for (int i = 1; i < myPolygon.size(); i++) {
+                    Point2D.Double current = myPolygon.get(i);
                     myDrawingTool.move(current.getX(), current.getY());
                 }
 
